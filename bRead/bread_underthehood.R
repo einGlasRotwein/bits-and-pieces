20e95380c08fa2ec0cc8bec7d5c69747062df2f5
einGlasRotwein <juliane.tkotz@gmail.com>
2020-03-31 13:28:16 +0200

    inserted space

--- bRead/bread_underthehood.R
+++ bRead/bread_underthehood.R
@@ -3,7 +3,7 @@
 
 # Create the dataframe's names.
 breads <- paste0("bread", 1:7, "_")
-breads <- paste0(breads, rep(1:5, each = length(breads)))
+breads <- paste0(breads, rep(1:6, each = length(breads)))
 
 types <- c("banana", "sourdough", "baguette", "toast", "brioche", "ciabatta")
 breads <- paste0(types, "_", rep(breads, each = length(types)))
@@ -17,7 +17,7 @@ names(df_bread) <- bread_names # set names
 df_bread[["baker"]] <- c("Gordon", "Juli", "rstats") # Let's have 3 bakers
 
 # All the columns that end with 1 will contain "B", all the ones that end with 2 will contain "R",
-# 3 = "E", 4 = "A", 5 = "D"
+# 3 = "E", 4 = "A", 5 = "D", 6 = " "
 
 # We get the names that end with 1 like this:
 grepl("_1$", names(df_bread)) # ... logical ...
@@ -32,7 +32,7 @@ df_bread[ , grepl("_2$", names(df_bread))] <- "R"
 df_bread[ , grepl("_3$", names(df_bread))] <- "E"
 df_bread[ , grepl("_4$", names(df_bread))] <- "A"
 df_bread[ , grepl("_5$", names(df_bread))] <- "D"
+df_bread[ , grepl("_6$", names(df_bread))] <- " " # empty column will make it nicer
 
 # Save as text file so it can just be loaded.
 write.csv(df_bread, file = "./bRead/df_bread.csv")
-
